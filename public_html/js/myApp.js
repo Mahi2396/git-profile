f49f823de1244451d2bb8282caa93b5a8bdb1fdb
mahesh computer <mahesh computer@mahesh>
2018-02-10 23:15:30 +0530

    1st commirt

---
+++ public_html/js/myApp.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function () {
+
+    $(window).resize(function () {
+        let leftNavWidth = document.getElementsByClassName("right-side-container")[0].clientWidth;
+        var elem = document.getElementById("navID");
+        elem.style.width = leftNavWidth - 32 + "px";
+        
+        let leftContainerWidth = document.getElementById("leftContainer").clientWidth;
+        var leftContainerElem = document.getElementById("profileOnscroll");
+         leftContainerElem.style.width = leftContainerWidth - 32 + "px";
+    });
+    $(window).scroll(function () {
+        let leftNavWidth = document.getElementsByClassName("right-side-container")[0].clientWidth;
+        var elem = document.getElementById("navID");
+        elem.style.width = leftNavWidth - 32 + "px";
+
+        let leftContainerWidth = document.getElementById("leftContainer").clientWidth;
+        var leftContainerElem = document.getElementById("profileOnscroll");
+         leftContainerElem.style.width = leftContainerWidth - 32 + "px";
+        
+        if (window.scrollY < 94) {
+            var elem = document.getElementById("navID");
+            elem.style.position = "static";
+            let elem1 = document.getElementById("pinnedRepositoriesContainerID");
+
+            elem1.style.margin = "0 0 24px 0";
+        } else {
+            var elem = document.getElementById("navID");
+            elem.style.position = "fixed";
+            let elem1 = document.getElementById("pinnedRepositoriesContainerID");
+
+            elem1.style.margin = "74px 0 24px 0";
+        }
+
+        var profileElem = document.getElementById("profileName");
+        if (window.scrollY >= profileElem.offsetTop + 94) {
+            var elem = document.getElementById("profileOnscroll");
+            elem.style.display = "block";
+             $(".show-on-scroll").addClass("dividerafter");
+        } else {
+              var elem = document.getElementById("profileOnscroll");
+            elem.style.display = "none";
+        }
+
+
+    });
+});
