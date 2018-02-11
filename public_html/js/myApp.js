68866ae746d80f6a02813bb43817a9fd102ca549
mahesh computer <mahesh computer@mahesh>
2018-02-11 13:41:01 +0530

    1st commirt

--- public_html/js/myApp.js
+++ public_html/js/myApp.js
@@ -7,14 +7,26 @@
 
 $(document).ready(function () {
 
+
+    $(".openNav").click(function () {
+        $(".header-menu").toggleClass("active-menu");
+        $(".top-menu-style").toggleClass("hide-menu");
+//do something
+    });
+    $(".top-menu-style").click(function () {
+        $(".header-menu").removeClass("active-menu");
+    });
+
+
+
     $(window).resize(function () {
         let leftNavWidth = document.getElementsByClassName("right-side-container")[0].clientWidth;
         var elem = document.getElementById("navID");
         elem.style.width = leftNavWidth - 32 + "px";
-        
+
         let leftContainerWidth = document.getElementById("leftContainer").clientWidth;
         var leftContainerElem = document.getElementById("profileOnscroll");
-         leftContainerElem.style.width = leftContainerWidth - 32 + "px";
+        leftContainerElem.style.width = leftContainerWidth - 32 + "px";
     });
     $(window).scroll(function () {
         let leftNavWidth = document.getElementsByClassName("right-side-container")[0].clientWidth;
@@ -23,8 +35,8 @@ $(document).ready(function () {
 
         let leftContainerWidth = document.getElementById("leftContainer").clientWidth;
         var leftContainerElem = document.getElementById("profileOnscroll");
-         leftContainerElem.style.width = leftContainerWidth - 32 + "px";
-        
+        leftContainerElem.style.width = leftContainerWidth - 32 + "px";
+
         if (window.scrollY < 94) {
             var elem = document.getElementById("navID");
             elem.style.position = "static";
@@ -43,9 +55,9 @@ $(document).ready(function () {
         if (window.scrollY >= profileElem.offsetTop + 94) {
             var elem = document.getElementById("profileOnscroll");
             elem.style.display = "block";
-             $(".show-on-scroll").addClass("dividerafter");
+            $(".show-on-scroll").addClass("dividerafter");
         } else {
-              var elem = document.getElementById("profileOnscroll");
+            var elem = document.getElementById("profileOnscroll");
             elem.style.display = "none";
         }
 
